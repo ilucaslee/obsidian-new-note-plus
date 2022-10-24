011ac7cb7e16013186bf30d5320665eee92abffc
lucaslee <ilucaslee@users.noreply.github.com>
2022-10-24 22:17:58 +0800

    bug fix https://github.com/obsidianmd/obsidian-releases/pull/1055#issuecomment-1205838499

--- main.ts
+++ main.ts
@@ -29,10 +29,15 @@ export default class NewNotePlus extends Plugin {
     async writeAndOpenFile(outputFileName: string, text: string, parameters: Parameters): Promise<TFile> {
         const file = this.app.vault.getAbstractFileByPath(outputFileName);
         if (!text) {
-            text = await navigator.clipboard.readText();
+            try {
+                text = await navigator.clipboard.readText();
+            }catch (e) {
+                text = '';
+            }
         }
-        if (file instanceof TFile) {
-            await this.app.vault.modify(file, text);
+        if (file instanceof TFile || file instanceof TFolder) {
+            // await this.app.vault.modify(file, text);
+            //TODO do sth to rename the new file name
         } else {
             const parts = outputFileName.split("/");
             const dir = parts.slice(0, parts.length - 1).join("/");
@@ -42,13 +47,12 @@ export default class NewNotePlus extends Plugin {
             await this.app.vault.create(outputFileName, text);
         }
 
-        var newPane = false;
-        await this.app.workspace.openLinkText(outputFileName, "", newPane, undefined);
+        await this.app.workspace.openLinkText(outputFileName, "", false, undefined);
         // this.app.workspace.getActiveViewOfType(MarkdownView)?.currentMode.clipboardManager.handlePaste;
 
         // await this.delay(this.settings.delayAfterFileOpening)
         let editor = this.getEditor();
-        await editor.focus();
+        editor.focus();
         let lastLineNum = editor.getDoc().lastLine();
         const lastLineLength = editor.getLine(lastLineNum).length;
         editor.setCursor(lastLineNum, lastLineLength);
